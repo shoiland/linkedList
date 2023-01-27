9b24b8a62b485b9e9438bd73b10026ef0f8ce08a
Scott <scott.hoiland87@gmail.com>
2023-01-27 11:23:34 -0600

    First commit

---
+++ src/ScottList.java
@@ -0,0 +1,47 @@
+public class ScottList<E> {
+
+    /* --------------------------- LOCAL CLASS  -----------------------------*/
+    private class Node<E>{
+        E data;
+        Node<E> next;
+
+        public Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    /* --------------------------- INSTANCE VARIABLES  -----------------------------*/
+    private Node<E> head;
+
+    /* --------------------------- CONSTRUCTOR  -----------------------------*/
+    public ScottList(){
+        this.head = null;
+    }
+
+    /* --------------------------- METHODS  -----------------------------*/
+    public void addToFront(E newData){
+        this.head = new Node<E>(newData, head);
+
+    }
+
+    public String toString(){
+        Node<E> current = head;
+        String result = "";
+
+        while (current != null) {
+            result = result + current.data.toString() + "\n";
+            current = current.next;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ScottList<String> dogNames = new ScottList();
+        dogNames.addToFront("Gatsby");
+        dogNames.addToFront("Mav");
+        dogNames.addToFront("June");
+        System.out.println(dogNames.toString());
+
+    }
+}
